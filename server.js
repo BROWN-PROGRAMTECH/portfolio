cacf335345fac561fafe0a26f2a5ba379dee4bf9
BROWN-PROGRAMTECH <aldenovpoutine99@gmail.com>
2023-08-06 01:13:43 +0100

    adding the path to the config method

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config({path: "config.env" })
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
