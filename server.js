9f3b71182571796152bf515f4d9c67a67a918be0
BROWN-PROGRAMTECH <aldenovpoutine99@gmail.com>
2023-12-08 03:55:49 +0100

    trying to solve cors issue

--- server.js
+++ server.js
@@ -26,10 +26,17 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser())
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}))
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true
+// }))
+const cors = require('Cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use(express.static(__dirname));
 
