dff95e4ad7532e6db7ba165cf437b9af2812a0e5
BROWN-PROGRAMTECH <39649631+BROWN-PROGRAMTECH@users.noreply.github.com>
2023-12-17 22:18:53 +0100

    Update server.js
    no change

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ dotenv.config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const cors = require('cors')
+const cors = require('cors');
 //const fileupload = require('express-fileupload'); 
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 
 const app = express() 
 
@@ -31,11 +34,11 @@ app.use(cookieParser())
 //     origin: 'http://localhost:3000',
 //     credentials: true
 // }))
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
 app.use(cors(corsOptions));
 
 app.use(express.static(__dirname));
